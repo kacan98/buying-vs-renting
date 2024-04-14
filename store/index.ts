43322efe3b4552dbe63d98c4da666dd3bc3eabc5
kacan98 <kacan@dynaway.com>
2024-04-14 14:04:16 +0200

    Add state to local storage

--- store/index.ts
+++ store/index.ts
@@ -1,18 +1,23 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import { buyingSlice } from "./calculatorSlices/buying.ts";
 import { futurePredictionsSlice } from "./calculatorSlices/futurePreditions.ts";
 import { rentingSlice } from "./calculatorSlices/renting.ts";
+import { loadFromLocalStorage, saveToLocalStorage } from "./localStorage.ts";
 
-const reducer = {
+const reducer = combineReducers({
   buying: buyingSlice.reducer,
   renting: rentingSlice.reducer,
   futurePredictions: futurePredictionsSlice.reducer,
-};
+});
+
+const persistedState = loadFromLocalStorage();
 
 export const store = configureStore({
   reducer,
+  preloadedState: persistedState,
 });
 
+store.subscribe(() => saveToLocalStorage(store.getState()));
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
 export type RootState = ReturnType<typeof store.getState>;
---
+++ store/localStorage.ts
@@ -0,0 +1,21 @@
+import { RootState } from "./index";
+
+export function saveToLocalStorage(state: RootState) {
+  try {
+    const serializedState = JSON.stringify(state);
+    localStorage.setItem("state", serializedState);
+  } catch (e) {
+    console.warn(e);
+  }
+}
+
+export function loadFromLocalStorage() {
+  try {
+    const serializedState = localStorage.getItem("state");
+    if (serializedState === null) return undefined;
+    return JSON.parse(serializedState);
+  } catch (e) {
+    console.warn(e);
+    return undefined;
+  }
+}
